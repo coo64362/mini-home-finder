67129edd488eaf28114b3ffa40a5ef9e95345ee6
Olive Obianagha <coo64362@uga.edu>
2026-05-25 16:11:41 -0400

    Add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <main>
-      <h1>MiniHomeFinder</h1>
-      <p>A mini real estate listing app built with React, Express, and MySQL.</p>
-    </main>
+    <>
+      <Navbar />
+
+     <main>
+        <h1>MiniHomeFinder</h1>
+        <p>A mini real estate listing app built with React, Express, and MySQL.</p>
+     </main>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <header>
+            <h2>
+                MiniHomeFinder
+            </h2>
+            <nav>
+                <a href="#home">Home</a>
+                <a href="#Listings">Listings</a>
+                <a href="#about">About</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
