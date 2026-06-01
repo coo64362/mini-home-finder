db9838da7244c6d89c3712ba9e832415984031fb
Olive Obianagha <coo64362@uga.edu>
2026-06-01 18:13:57 -0400

    Style hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 function Hero() {
     return (
-        <section id="home">
-            <p>Real Estate Search made simple</p>
+        <section id="home" className="hero">
+            <p className="eyebrow">Real Estate Search made simple</p>
 
             <h1>Find properties that match your needs.</h1>
 
-            <p>
+            <p className="hero-text">
                 MiniHomeFinder is a full-stack real estate listing app built to practice 
                 React, Express, MySQL, and testing. 
             </p>
 
-            <a href="#Listings">View Listings</a>
+            <a className="primary-button"href="#Listings">View Listings</a>
         </section>
     );
 }
