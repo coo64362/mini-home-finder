63c0e8326332b2575d15e09ec5c1cb19b70a6dd9
Olive Obianagha <coo64362@uga.edu>
2026-06-01 17:29:44 -0400

    Style navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 function Navbar() {
     return (
-        <header>
-            <h2>
+        <header className="navbar">
+            <h2 className="logo">
                 MiniHomeFinder
             </h2>
-            <nav>
+            <nav className="nav-links">
                 <a href="#home">Home</a>
                 <a href="#Listings">Listings</a>
                 <a href="#about">About</a>
