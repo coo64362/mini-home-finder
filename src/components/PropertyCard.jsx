16599351d9917bd0080a878fc6269ca6ba41cb0c
Olive Obianagha <coo64362@uga.edu>
2026-06-04 01:48:47 -0400

    Add property card component

---
+++ src/components/PropertyCard.jsx
@@ -0,0 +1,29 @@
+function PropertyCard({property}) {
+    return (
+        <article className="property-card">
+            <img
+                src={property.imageUrl}
+                alt={property.title}
+                className="property-image"
+            />
+
+            <div className="property-content">
+                <h3>{property.title}</h3>
+
+                <p className="propery-content">
+                    {property.city}, {property.state} {property.zip}
+                </p>
+
+                <p className="property-price">${property.price.toLocaleString()}</p>
+
+                <p className="property-details">
+                    {property.beds} beds * {property.baths} baths *{" "}
+                    {property.squareFeet.toLocaleString()} sq ft
+                </p>
+
+                <p>{property.description}</p>
+            </div>
+        </article>
+    );
+}
+
