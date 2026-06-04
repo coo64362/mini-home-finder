83af3d42b89bd159cc7e02ae177f811bc31539f8
Olive Obianagha <coo64362@uga.edu>
2026-06-04 16:30:51 -0400

    Add properties API route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import {properties} from "./data/properties.js";
 
 const app = express();
 const PORT = 5001;
@@ -9,6 +10,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("MiniHomeFinder API is running");
+});
+
+app.get("/api/properties", (req, res) => {
+    res.json(properties);
 });
 
 app.listen(PORT, () => {
