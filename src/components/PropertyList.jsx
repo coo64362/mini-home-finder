31e674faeb8c136d111f0a94b49ccb9c778db4c5
Olive Obianagha <coo64362@uga.edu>
2026-06-04 10:44:58 -0400

    Add search by city and zip

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -1,14 +1,33 @@
+import {useState} from "react";
 import {properties} from "../data/properties";
 import PropertyCard from "./PropertyCard";
 
 function PropertyList() {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredProperties = properties.filter((property) => {
+        const searchText = searchTerm.toLowerCase();
+
+        return (
+            property.city.toLowerCase().includes(searchText) ||
+            property.zip.includes(searchText)
+        );
+    });
     return (
         <section id="listings" className="section">
             <h2>Property Listings</h2>
             <p>Browse available properties below.</p>
 
+            <input
+                type="text"
+                placeholder="Search by city or ZIP"
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+            />
+
+
             <div className="property-grid">
-                {properties.map((property) => (
+                {filteredProperties.map((property) => (
                     <PropertyCard key={property.id} property={property}/>
                 ))}
             </div>
